8e101400508f87089f0404e7fe10a6eddcba3360
abiramen <anadarajah@atlassian.com>
2024-06-05 16:10:10 +1000

    tut01 code

--- week01-files/factorial.c
+++ week01-files/factorial.c
@@ -3,14 +3,18 @@
 int factorial(int n);
 
 int main(void) {
-    int n;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-    printf("%d! = %d\n", n, factorial(n));
-    return 0;
+  int n;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  printf("%d! = %d\n", n, factorial(n));
+  return 0;
 }
 
 int factorial(int n) {
-    // todo
-    return -1;
+  if (n == 0) {
+    // base case
+    return 1;
+  }
+  // recursive case
+  return n * factorial(n - 1);
 }
